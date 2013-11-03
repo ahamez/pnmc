8cf34cd613531330ca0266e2383f8986c4196018
Alexandre Hamez <alexandre.hamez@gmail.com>
2013-11-03 11:03:49 +0100

    Hand-written parser for TINA format.
    Get rid of one of the two boost::spirit dependencies. Some features are still lacking, like ‘’K on markings and other arc types.

--- src/parsers/tina.cc
+++ src/parsers/tina.cc
@@ -1,11 +1,6 @@
+#include <algorithm>
 #include <istream>
-
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_bind.hpp>
-#include <boost/spirit/include/phoenix_statement.hpp>
-#include <boost/spirit/include/phoenix_object.hpp>
+#include <sstream>
 
 #include "parsers/tina.hh"
 #include "pn/arc.hh"
@@ -14,116 +9,65 @@ namespace pnmc { namespace parsers {
 
 /*------------------------------------------------------------------------------------------------*/
 
-namespace ascii = boost::spirit::ascii;
-namespace phoenix = boost::phoenix;
-namespace qi = boost::spirit::qi;
+struct parse_error final
+  : public std::exception
+{
+public:
+
+  ~parse_error()
+  noexcept
+  {}
+
+  const char*
+  what()
+  const noexcept
+  {
+    return "";
+  }
+};
 
 /*------------------------------------------------------------------------------------------------*/
 
-template <typename Iterator>
-struct tina_parser
-  : qi::grammar<Iterator, void(), ascii::space_type>
+std::pair<std::string, unsigned int>
+place_valuation(const std::string& s)
 {
-  qi::rule< Iterator, void(), ascii::space_type>
-          start;
-
-  qi::rule< Iterator, std::string()>
-          id;
-
-  qi::rule< Iterator, std::string(), ascii::space_type>
-          label;
-
-  qi::rule< Iterator, void(), qi::locals<std::string, std::string>
-          , ascii::space_type>
-          place;
-
-  qi::rule< Iterator, void(), ascii::space_type>
-          time_interval;
-
-  qi::rule< Iterator, pn::arc(), ascii::space_type>
-          arc;
-
-  qi::rule< Iterator, unsigned int(), ascii::space_type>
-          valuation;
-
-  qi::rule< Iterator, void(), qi::locals<std::string, std::string, pn::arc, std::string>
-          , ascii::space_type>
-          transition;
-
-  qi::rule< Iterator, void(), ascii::space_type>
-          net;
-
-  tina_parser(pn::net& n)
-    : tina_parser::base_type(start)
+  const auto star_cit = std::find(s.cbegin(), s.cend(), '*');
+  if (star_cit == s.cend())
   {
-    using ascii::char_;
-
-    using phoenix::bind;
-    using phoenix::construct;
-
-    using qi::_1;
-    using qi::_a;
-    using qi::_b;
-    using qi::_c;
-    using qi::_d;
-    using qi::_val;
-    using qi::eps;
-    using qi::space;
-    using qi::lexeme;
-    using qi::lit;
-    using qi::uint_;
-
-    id %= +(qi::alpha|qi::digit|'\''|'_');
-
-    label %= lit(':') >> id;
-
-    place =
-       lit("pl")
-    >> id[_a = _1]
-    >> -label[_b = _1]
-    >> lit('(')
-    >> uint_[bind(&pn::net::add_place, n, _a, _b, _1)]
-    >> lit(')')
-    ;
-
-    time_interval =
-       (lit('[') | lit(']'))
-    >> uint_
-    >> lit(',')
-    >> (uint_ | lit('w'))
-    >> (lit('[') | lit(']'))
-    ;
-
-    valuation = uint_[_val = _1] >> -(lit('K')[_val *= 1000]|lit('M')[_val *= 1000000]);
-
-    arc = ( lit('*') | lit('?') | lit("?-") | lit('!') | lit("!-") )
-          >> valuation [_val = construct<pn::arc>(_1)];
-
-    transition =
-       lit("tr")
-    >> id[_a = _1]
-    >> eps[_d = ""]
-    >> -label[_d = _1]
-    >> eps[bind(&pn::net::add_transition, n, _a, _d)]
-    >> -time_interval
-    >> *(id     [_b = _1]
-        >> eps    [_c = construct<pn::arc>()] // default arc if no one is specified
-        >> -arc [_c = _1]               // an arc was found, use it
-        )         [bind(&pn::net::add_pre_place, n, _a, _b, _c)]
-    >> lit("->")
-    >> *((id      [_b=_1]
-          >> eps    [_c=construct<pn::arc>()]
-          >> -arc [_c=_1]
-         )          [bind(&pn::net::add_post_place, n, _a, _b, _c)]
-         - lexeme[((lit("pl") |lit("tr")|lit("net")) >> +space)]
-        )
-    ;
-
-    net = lit("net") >> id/*[bind(&net::name, n, _1)]*/;
-
-    start = +(place | transition | net);
+    return std::make_pair(s, 1);
   }
-};
+  try
+  {
+    const auto valuation = std::stoi(std::string(star_cit + 1, s.cend()));
+    return std::make_pair(std::string(s.cbegin(), star_cit), valuation);
+  }
+  catch (const std::invalid_argument&)
+  {
+    throw parse_error();
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+unsigned int
+marking(const std::string& s)
+{
+  if (*s.cbegin() == '(' and *std::prev(s.cend()) == ')')
+  {
+    try
+    {
+      return std::stoi(std::string(std::next(s.cbegin()), std::prev(s.cend())));
+    }
+    catch (const std::invalid_argument&)
+    {
+      throw parse_error();
+    }
+  }
+  else
+  {
+    throw parse_error();
+  }
+}
 
 /*------------------------------------------------------------------------------------------------*/
 
@@ -131,20 +75,90 @@ std::shared_ptr<pn::net>
 tina(std::istream& in)
 {
   std::shared_ptr<pn::net> net_ptr = std::make_shared<pn::net>();
+  auto& net = *net_ptr;
 
-  // Don't skip whitespaces, the parser will do it.
-  in.unsetf(std::ios::skipws);
-
-  boost::spirit::istream_iterator cit(in);
-  boost::spirit::istream_iterator end;
-
-  tina_parser<boost::spirit::istream_iterator> parser(*net_ptr);
-
-  const bool r = qi::phrase_parse(cit, end, parser, ascii::space);
-
-  if (not r or cit != end)
+  try
   {
-    // Parse failed.
+    std::string line, s0, s1, s2;
+    line.reserve(1024);
+
+    while (std::getline(in, line))
+    {
+      std::istringstream ss(line);
+
+      // Empty line or comment.
+      if (((ss >> std::ws).peek() == '#') or not (ss >> s0))
+      {
+        continue;
+      }
+
+      // Net
+      if (s0 == "net")
+      {
+        continue;
+      }
+
+      // Transitions
+      else if (s0 == "tr")
+      {
+        std::string place_id;
+        unsigned int valuation;
+
+        if (ss >> s0)
+        {
+          net.add_transition(s0, "");
+        }
+        else
+        {
+          throw parse_error();
+        }
+
+        // Skip time interval, if any.
+        const auto c = (ss >> std::ws).peek();
+        if (c == '[' or c == ']')
+        {
+          ss >> s1;
+        }
+
+        while (ss >> s1)
+        {
+          if (s1 == "->")
+          {
+            break;
+          }
+          std::tie(place_id, valuation) = place_valuation(s1);
+          net.add_pre_place(s0, place_id, pn::arc(valuation));
+        }
+
+        while (ss >> s1)
+        {
+          std::tie(place_id, valuation) = place_valuation(s1);
+          net.add_post_place(s0, place_id, pn::arc(valuation));
+        }
+      }
+
+      // Places
+      else if (s0 == "pl")
+      {
+        if (ss >> s1 >> s2)
+        {
+          net.add_place(s1, "", marking(s2));
+        }
+        else
+        {
+          throw parse_error();
+        }
+      }
+
+      // Error.
+      else
+      {
+        throw parse_error();
+      }
+    }
+  }
+  catch (const parse_error&)
+  {
     return nullptr;
   }
 
