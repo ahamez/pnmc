5e1491358f833c1077cdf4bd3f82715b52aa0a05
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-03-04 15:06:32 +0100

    Make bound_error inherit from sdd::interrupt.

--- src/mc/bound_error.hh
+++ src/mc/bound_error.hh
@@ -1,20 +1,28 @@
+#ifndef _PNMC_MC_BOUNDED_ERROR_HH_
+#define _PNMC_MC_BOUNDED_ERROR_HH_
+
 #include <exception>
 #include <string>
+
+#include <sdd/hom/interrupt.hh>
 
 namespace pnmc { namespace mc {
 
 /*------------------------------------------------------------------------------------------------*/
 
+template <typename C>
 struct bound_error final
-  : public std::exception
+  : public sdd::interrupt<C>
 {
   const std::string& place;
 
   bound_error(const std::string& p)
-    : place(p)
+    : sdd::interrupt<C>(), place(p)
   {}
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
 }} // namespace pnmc::mc
+
+#endif // _PNMC_MC_BOUNDED_ERROR_HH_
