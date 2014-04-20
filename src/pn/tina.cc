416a24946c2a7f347a16eaab5c5e0237d7d09720
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-04-20 10:33:21 +0200

    TINA doesn't like dots in identifiers.

--- src/pn/tina.cc
+++ src/pn/tina.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <ostream>
 
 #include "pn/tina.hh"
@@ -6,27 +7,43 @@ namespace pnmc { namespace pn {
 
 /*------------------------------------------------------------------------------------------------*/
 
+namespace /* anonymous */ {
+
+std::string
+format(const std::string& str)
+{
+  std::string res;
+  res.reserve(str.size());
+  std::transform( str.cbegin(), str.cend(), std::back_inserter(res)
+                , [](char c){if (c == '.') return '_'; else return c;});
+  return res;
+}
+
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
 void
 tina(std::ostream& out, const net& net)
 {
   for (const auto& transition : net.transitions())
   {
-    out << "tr t" << transition.id;
+    out << "tr t" << format(transition.id);
     for (const auto& arc : transition.pre)
     {
-      out << " p" << arc.first;
+      out << " p" << format(arc.first);
     }
     out << " ->";
     for (const auto& arc : transition.post)
     {
-      out << " p" << arc.first;
+      out << " p" << format(arc.first);
     }
     out << "\n";
   }
 
   for (const auto& place : net.places())
   {
-    out << "pl p" << place.id << " (" << place.marking << ")\n";
+    out << "pl p" << format(place.id) << " (" << place.marking << ")\n";
   }
 
   out << "net generated_net\n";
