3f7697d126aa47b169f9a32a79d9b4ba691e5fc1
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-07-16 13:54:08 +0200

    Header-only dead homomorphism.

--- src/mc/classic/dead.hh
+++ src/mc/classic/dead.hh
@@ -2,10 +2,10 @@
 #define _PNMC_MC_DEAD_HH_
 
 #include <functional> // hash
-#include <iosfwd>
+#include <ostream>
 
+#include <sdd/util/hash.hh>
 #include "sdd/values/flat_set.hh"
-#include "sdd/values_manager.hh"
 
 namespace pnmc { namespace mc { namespace classic {
 
@@ -15,20 +15,42 @@ struct dead
 {
   const unsigned int valuation;
 
-  dead(unsigned int);
+  dead(unsigned int v)
+    : valuation(v)
+  {}
 
   sdd::values::flat_set<unsigned int>
-  operator()(const sdd::values::flat_set<unsigned int>&) const;
+  operator()(const sdd::values::flat_set<unsigned int>& val)
+  const
+  {
+    sdd::values::values_traits<sdd::values::flat_set<unsigned int>>::builder builder;
+    for (const auto v : val)
+    {
+      if (v < valuation)
+      {
+        builder.insert(v);
+      }
+    }
+    return std::move(builder);
+  }
+
+  /// @brief Equality of two dead.
+  friend
+  bool
+  operator==(const dead& lhs, const dead& rhs)
+  noexcept
+  {
+    return lhs.valuation == rhs.valuation;
+  }
+
+  /// @brief Textual output of a dead.
+  friend
+  std::ostream&
+  operator<<(std::ostream& os, const dead& d)
+  {
+    return os << "dead(" << d.valuation << ")";
+  }
 };
-
-/// @brief Equality of two dead.
-bool
-operator==(const dead&, const dead&)
-noexcept;
-
-/// @brief Textual output of a dead.
-std::ostream&
-operator<<(std::ostream&, const dead&);
 
 /*------------------------------------------------------------------------------------------------*/
 
@@ -42,7 +64,12 @@ namespace std
 template <>
 struct hash<pnmc::mc::classic::dead>
 {
-  std::size_t operator()(const pnmc::mc::classic::dead&) const noexcept;
+  std::size_t
+  operator()(const pnmc::mc::classic::dead& d)
+  const noexcept
+  {
+    return sdd::util::hash(d.valuation);
+  }
 };
 
 /*------------------------------------------------------------------------------------------------*/
