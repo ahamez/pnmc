866b97452825de670cc73bddd85af8c41cd24af2
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-09-15 19:18:29 +0200

    Resilient Tina parser.
    Using a (sort of) recursive descent parser.

--- src/parsers/tina.cc
+++ src/parsers/tina.cc
@@ -1,7 +1,13 @@
 #include <algorithm>
+#include <array>
+#include <cassert>
 #include <iostream>
-#include <sstream>
+#include <limits>
+#include <regex>
 #include <string>
+#include <type_traits>
+
+#include <boost/optional.hpp>
 
 #include "parsers/parse_error.hh"
 #include "parsers/tina.hh"
@@ -12,113 +18,414 @@ namespace {
 
 /*------------------------------------------------------------------------------------------------*/
 
-unsigned int
-value(std::string::const_iterator cit, std::string::const_iterator cend)
+// @brief Used to start keywords in token_t at indexes higher than other regular tokens matched by 
+// the regular expression.
+static constexpr auto keywords_gap = 1000u;
+
+// @brief Start at 1 because the relative position in the enum is used to compute the type of the 
+// token and because submatches of regular expressions start at 1 (0 is the whole match).
+enum class token_t { skip = 1u, newline, number, qname, name, colon, comma
+                   , lbracket, rbracket, arrow, lparen, rparen, question, exclamation, minus, mult
+                   , comment
+                   , net = keywords_gap, transition, place
+                   , eof};
+
+/*------------------------------------------------------------------------------------------------*/
+
+/// @brief Gets the position of tk in the enum class token_t.
+std::size_t
+pos(token_t tk)
 {
-  std::string s(cit, cend);
-  try
-  {
-    std::size_t pos;
-    auto value = std::stoi(s, &pos);
-    std::advance(cit, pos);
-    if (std::distance(cit, cend) == 1)
-    {
-      switch (*cit)
-      {
-        case 'K': value *= 1000; break;
-        case 'M': value *= 1000000; break;
-        default: throw parse_error("Invalid suffix, got '" + std::string(cit, cend) + "'");
-      }
-    }
-    else if (std::distance(cit, cend) > 1)
-    {
-      throw parse_error("Invalid suffix, got " + std::string(cit, cend));
-    }
-    return value;
-  }
-  catch (const std::invalid_argument&)
-  {
-    throw parse_error("Expected a value, got " + s);
-  }
+  return static_cast<std::underlying_type_t<token_t>>(tk);
 }
 
 /*------------------------------------------------------------------------------------------------*/
 
-std::pair<std::string, pn::arc>
-place_arc(const std::string& s)
+/// @brief Describes a token (its type and its string value).
+struct token
 {
-  auto pos = s.find_first_of("*?!");
-  auto arc_type = pn::arc::type::normal;
+  token_t ty;
+  std::string val;
+  std::size_t line;
+  std::size_t column;
 
-  if (pos == std::string::npos)
+  std::string
+  str()
+  const
   {
-    return std::make_pair(s, pn::arc{1, arc_type});
+    return "'" + val + "' at " + std::to_string(line) + ":" + std::to_string(column);
   }
+};
 
-  if (pos == s.length() - 1)
+/*------------------------------------------------------------------------------------------------*/
+
+/// @brief Returns the tokens of a Tina model.
+template <typename InputIterator>
+auto
+tokens(InputIterator&& begin, InputIterator&& end)
+{
+  static constexpr std::array<const char*, 3> keywords = {{"net", "tr", "pl"}};
+  // Order must match enum token_t
+  static const std::regex regex
+  ( 
+    "([ \\t]+)|"                  // skip spaces and tabs
+    "(\\n)|"                      // newline (to keep track of line numbers)
+    "(\\d+[KM]?)|"                // numbers can have a suffix
+    "(\\{)|"                      // only search for opening brace for qualified names
+    "([a-zA-Z'_][a-zA-Z0-9'_]*)|" // don't begin by a number, we add it if necessary in the parser
+    "(:)|"                        // colon
+    "(,)|"                        // comma
+    "(\\[)|"                      // left bracket
+    "(\\])|"                      // right bracket
+    "(->)|"                       // arrow
+    "(\\()|"                      // left parenthesis
+    "(\\))|"                      // right parenthesis
+    "(\\?)|"                      // question mark
+    "(!)|"                        // exclamation mark
+    "(-)|"                        // minus
+    "(\\*)|"                      // multiplication
+    "(^#[^\\n]*)"                 // comments start at the beginning of a line
+  );
+
+  // To report error location, if any.
+  auto line = 1ul;
+  auto column = 1ul;
+  std::smatch match;
+  bool last_match_is_newline = true;
+  std::deque<token> tokens;
+
+  while (true)
   {
-    throw parse_error("Valuation expected, got '" + s + "'");
-  }
+    if (begin == end)
+    {
+      tokens.emplace_back(token {token_t::eof, "", line, column});
+      break;
+    }
 
-  auto valuation_pos = pos;
-  switch (s[pos])
-  {
-      case '*': break;
+    std::regex_search(begin, end, match, regex);
 
-      case '?':
+    if (match.position() != 0)
+    {
+      throw parse_error( "Unexpected '" + match.prefix().str() + "' at " + std::to_string(line)
+                       + ':' + std::to_string(column));
+    }
+
+    column += match.length();
+    begin += match.length();
+
+    if (match[pos(token_t::skip)].matched)
+    {
+      continue;
+    }
+
+    if (match[pos(token_t::newline)].matched)
+    {
+      last_match_is_newline = true;
+      column = 1;
+      line += 1;
+      continue;
+    }
+
+    if (match[pos(token_t::comment)].matched)
+    {
+      if (not last_match_is_newline)
       {
-        if (s[pos + 1] == '-')
+        throw parse_error("Invalid comment at line " + std::to_string(line));
+      }
+      continue;
+    }
+    last_match_is_newline = false;
+
+    // Get the type of token by looking for the first successful submatch.
+    const auto ty = [&match]
+    {
+      auto t = pos(token_t::number);
+      for (; t < match.size(); ++t)
+      {
+        if (match[t].matched) {break;}
+      }
+      return t;
+    }();
+    assert(ty < match.size());
+
+    // Check if name is a keyword.
+    if (ty == pos(token_t::name))
+    {
+      const auto search = std::find(keywords.cbegin(), keywords.cend(), match[ty].str());
+      if (search != keywords.cend())
+      {
+        const auto kw_ty = std::distance(keywords.cbegin(), search) + keywords_gap;
+        tokens.emplace_back(token {token_t(kw_ty), "", line, column});
+        continue;
+      }
+    }
+    // Process qualified names manually (to handle despecialization).
+    else if (ty == pos(token_t::qname))
+    {
+      std::string qname;
+      qname.reserve(32);
+      bool despecialization = false;
+      while (true)
+      {
+        if (despecialization)
         {
-          ++valuation_pos;
-          arc_type = pn::arc::type::inhibitor;
+          qname.push_back(*begin);
+          despecialization = false;
         }
         else
         {
-          arc_type = pn::arc::type::read;
+          if      (*begin == '}')  {++begin; ++column; break;}
+          else if (*begin == '\\') {despecialization = true;}
+          else                     {qname.push_back(*begin);}
         }
-        break;
+        ++begin;
+        ++column;
       }
-
-      case '!':
-      {
-        if (s[pos +1] == '-')
-        {
-          ++valuation_pos;
-          arc_type = pn::arc::type::stopwatch_inhibitor;
-        }
-        else
-        {
-          arc_type = pn::arc::type::stopwatch;
-        }
-        break;
-      }
-
-      default :
-      {
-        std::stringstream ss;
-        ss << "Expected '*', '?' or '!', got '" << s[pos] << "'";
-        throw parse_error(ss.str());
-      }
+      tokens.emplace_back(token {token_t::qname, qname, line, column});
+      continue;
+    }
+    // All other tokens.
+    tokens.emplace_back(token {token_t(ty), match[ty].str(), line, column});
   }
 
-  const auto valuation = value(s.cbegin() + valuation_pos + 1, s.cend());
-  return std::make_pair(s.substr(0, pos), pn::arc{valuation, arc_type});
+  return tokens;
 }
 
 /*------------------------------------------------------------------------------------------------*/
 
-unsigned int
-marking(const std::string& s)
+struct parse_cxt
 {
-  if (*s.cbegin() == '(' and *std::prev(s.cend()) == ')')
+  std::deque<token>::const_iterator curr;
+  auto val()      const noexcept {return (curr - 1)->val;}
+  auto previous() const noexcept {return *(curr - 1);}
+  auto current()  const noexcept {return *curr;}
+  void advance()  noexcept       {++curr;}
+};
+
+/*------------------------------------------------------------------------------------------------*/
+
+bool
+accept(parse_cxt& cxt, token_t ty)
+noexcept
+{
+  return cxt.curr->ty == ty ? cxt.advance(), true : false;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+void
+error(parse_cxt& cxt)
+{
+  throw parse_error("Unexpected " + cxt.current().str());
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+bool
+expect(parse_cxt& cxt, token_t ty)
+{
+  if   (accept(cxt, ty)) {return true;}
+  else                   {error(cxt); __builtin_unreachable();}
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+boost::optional<std::string>
+accept_name(parse_cxt& cxt)
+{
+  if (accept(cxt, token_t::number)) // identifiers may start with a number
   {
-    return value(std::next(s.cbegin()), std::prev(s.cend()));
+    const auto name = cxt.val();
+    accept(cxt, token_t::name);
+    return name + cxt.val();
+  }
+  else if (accept(cxt, token_t::name))
+  {
+    return cxt.val();
+  }
+  return {};
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+std::string
+id(parse_cxt& cxt)
+{
+  if (const auto maybe_name = accept_name(cxt))
+  {
+    return *maybe_name;
   }
   else
   {
-    throw parse_error("Invalid marking format: " + s);
+    expect(cxt, token_t::qname);
+    return cxt.val();
   }
 }
+
+/*------------------------------------------------------------------------------------------------*/
+
+unsigned int
+number(parse_cxt& cxt)
+{
+  expect(cxt, token_t::number);
+  const auto res = std::stoi(cxt.val());
+  switch (cxt.val().back())
+  {
+    case 'K': return res * 1'000;
+    case 'M': return res * 1'000'000;
+    default : return res;
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+boost::optional<std::string>
+target(parse_cxt& cxt)
+{
+  if      (const auto maybe_name = accept_name(cxt)) {return *maybe_name;}
+  else if (accept(cxt, token_t::qname))              {return cxt.val();}
+  else                                               {return {};}
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+template <typename Fun>
+void
+input_arcs(parse_cxt& cxt, Fun&& add_arc)
+{
+  while (true)
+  {
+    const auto maybe_target = target(cxt);
+    if (not maybe_target) {break;} // no more arcs
+
+    pn::arc::type arc_ty = pn::arc::type::normal;
+    auto valuation = 1u;
+
+    if (accept(cxt, token_t::mult))
+    {
+      valuation = number(cxt);
+    }
+    else if (accept(cxt, token_t::question))
+    {
+      if (accept(cxt, token_t::minus)) {arc_ty = pn::arc::type::inhibitor;}
+      else                             {arc_ty = pn::arc::type::read;}
+      valuation = number(cxt);
+    }
+    else if (accept(cxt, token_t::exclamation))
+    {
+      if (accept(cxt, token_t::minus)) {arc_ty = pn::arc::type::stopwatch_inhibitor;}
+      else                             {arc_ty = pn::arc::type::stopwatch;}
+      valuation = number(cxt);
+    }
+    add_arc(*maybe_target, pn::arc{valuation, arc_ty});
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+template <typename Fun>
+void
+output_arcs(parse_cxt& cxt, Fun&& add_arc)
+{
+  while (true)
+  {
+    const auto maybe_target = target(cxt);
+    if (not maybe_target)
+    {
+      break; // no more arcs
+    }
+    auto valuation = 1u;
+    if (accept(cxt, token_t::mult))
+    {
+      valuation = number(cxt);
+    }
+    add_arc(*maybe_target, pn::arc{valuation, pn::arc::type::normal});
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+void
+transition(parse_cxt& cxt, pn::net& n)
+{
+  const auto tid = id(cxt);
+  n.add_transition(tid);
+
+  if (accept(cxt, token_t::colon)) // label
+  {
+    id(cxt);
+  }
+
+  if (accept(cxt, token_t::lbracket) or accept(cxt, token_t::rbracket)) // time interval
+  {
+    static constexpr auto inf = std::numeric_limits<unsigned int>::max();
+
+    // To report error location.
+    const auto line = cxt.previous().line;
+    const auto column = cxt.previous().column - 1;
+
+    const bool open_lower_endpoint = cxt.val() == "]";
+    bool open_upper_endpoint = false;
+
+    auto low = number(cxt);
+
+    expect(cxt, token_t::comma);
+
+    auto high = 0u;
+    if (accept(cxt, token_t::name))
+    {
+      if (cxt.val() != "w") {error(cxt);}
+      high = inf;
+    }
+    else
+    {
+      high = number(cxt);
+    }
+
+    if (not accept(cxt, token_t::rbracket))
+    {
+      expect(cxt, token_t::lbracket);
+      open_upper_endpoint = cxt.val() == "[";
+    }
+
+    if (open_lower_endpoint)                 {low += 1;}
+    if (open_upper_endpoint and high != inf) {high -= 1;}
+
+    if (high < low)
+    {
+      throw parse_error( "Time interval at " + std::to_string(line) + ":" + std::to_string(column));
+    }
+
+    n.add_time_interval(tid, low, high);
+  }
+
+  input_arcs(cxt, [&](auto&& pre, auto&& arc){n.add_pre_place(tid, pre, arc);});
+  if (accept(cxt, token_t::arrow))
+  {
+    output_arcs(cxt, [&](auto&& post, auto&& arc){n.add_post_place(tid, post, arc);});
+  }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+void
+place(parse_cxt& cxt, pn::net& n)
+{
+  const auto name = id(cxt);
+  auto marking = 0u;
+  if (accept(cxt, token_t::colon)) // label
+  {
+    id(cxt);
+  }
+  if (accept(cxt, token_t::lparen)) // initial marking
+  {
+    marking = number(cxt);
+    expect(cxt, token_t::rparen);
+  }
+  n.add_place(name, marking);
+}
+
+/*------------------------------------------------------------------------------------------------*/
 
 } // namespace anonymous
 
@@ -127,194 +434,20 @@ marking(const std::string& s)
 std::shared_ptr<pn::net>
 tina(std::istream& in)
 {
-  // Known limitations:
-  // - there's must be a transition or place per line
-  // - labels are ignored
-  // - time intervals are not completely validated (e.g. [0, 2a] is wrongly validated
-
   std::shared_ptr<pn::net> net_ptr = std::make_shared<pn::net>();
-  auto& net = *net_ptr;
 
-  std::string line, s0, s1, s2, ignore;
-  line.reserve(1024);
+  std::string text{std::istreambuf_iterator<char>{in}, std::istreambuf_iterator<char>{}};
 
-  while (std::getline(in, line))
+  const auto tks = tokens(text.cbegin(), text.cend());
+  parse_cxt cxt {tks.cbegin()};
+
+  while (cxt.current().ty != token_t::eof)
   {
-    std::istringstream ss(line);
-
-    // Empty line or comment.
-    if (((ss >> std::ws).peek() == std::char_traits<char>::to_int_type('#')) or not (ss >> s0))
-    {
-      continue;
-    }
-
-    // Net
-    if (s0 == "net")
-    {
-      ss >> s0;
-      net_ptr->name = s0;
-    }
-
-    // Transitions
-    else if (s0 == "tr")
-    {
-      std::string place_id;
-
-      if (ss >> s0)
-      {
-        net.add_transition(s0);
-      }
-      else
-      {
-        throw parse_error("Invalid transition: " + line);
-      }
-
-      // Ignore label, if any.
-      if ((ss >> std::ws).peek() == std::char_traits<char>::to_int_type(':'))
-      {
-        ss >> ignore >> ignore; // ':' <label>
-      }
-
-      // Time interval.
-      const auto peek = (ss >> std::ws).peek();
-      if (   peek == std::char_traits<char>::to_int_type('[')
-          or peek == std::char_traits<char>::to_int_type(']'))
-      {
-        s1.clear();
-
-        const bool open_lower_endpoint = ss.get() == ']';
-        bool open_upper_endpoint = false;
-
-        while (ss.good())
-        {
-          auto c = ss.get();
-          if (c == '[')
-          {
-            open_upper_endpoint = true;
-            break;
-          }
-          if (c == ']')
-          {
-            break;
-          }
-          s1.push_back(c);
-        }
-
-        if (not ss.good())
-        {
-          throw parse_error("Unexpected end of file. Missing '[' or ']'?");
-        }
-
-        std::istringstream ss1(s1);
-        std::getline(ss1, s2, ','); // split on ','
-        unsigned int first, last;
-        try
-        {
-          first = std::stoi(s2);
-        }
-        catch (const std::invalid_argument&)
-        {
-          throw parse_error("Expected a value, got: '" + s2 + "'");
-        }
-        ss1 >> s2;
-        try
-        {
-
-          last = std::stoi(s2);
-          const auto old_first = first;
-          const auto old_last = last;
-
-          if (open_lower_endpoint)
-          {
-            first += 1;
-          }
-
-          if (open_upper_endpoint)
-          {
-            last -= 1;
-          }
-
-          if (last < first)
-          {
-            throw parse_error( "Invalid time interval '"
-                             + (open_lower_endpoint ? std::string("]") : std::string("["))
-                             + std::to_string(old_first) + "," + std::to_string(old_last)
-                             + (open_upper_endpoint ? std::string("[") : std::string("]"))
-                             + "'");
-          }
-        }
-        catch (const std::invalid_argument& e)
-        {
-          if (s2 == "w")
-          {
-            last = std::numeric_limits<unsigned int>::max();
-          }
-          else
-          {
-            throw parse_error("Expected a value, got: '" + s2 + "'");
-          }
-        }
-
-        net_ptr->add_time_interval(s0, first, last);
-      }
-
-      bool found_arrow = false;
-      while (ss >> s1)
-      {
-        if (s1 == "->")
-        {
-          found_arrow = true;
-          break;
-        }
-        const auto res = place_arc(s1);
-        net.add_pre_place(s0, res.first, res.second);
-      }
-
-      if (not found_arrow)
-      {
-        throw parse_error("Invalid transition (missing '->'): " + line);
-      }
-
-      while (ss >> s1)
-      {
-        const auto res = place_arc(s1);
-        net.add_post_place(s0, res.first, res.second);
-      }
-    }
-
-    // Places
-    else if (s0 == "pl")
-    {
-      if (ss >> s1)
-      {
-        // Ignore label, if any.
-        if ((ss >> std::ws).peek() == std::char_traits<char>::to_int_type(':'))
-        {
-          ss >> ignore >> ignore; // ':' <label>
-        }
-
-
-        if (ss >> s2)
-        {
-          net.add_place(s1, marking(s2));
-        }
-        else
-        {
-          net.add_place(s1, 0);
-        }
-      }
-      else
-      {
-        throw parse_error("Invalid place: " + line);
-      }
-    }
-
-    // Error.
-    else
-    {
-      throw parse_error("Invalid line: " + line);
-    }
-  }
+    if      (accept(cxt, token_t::net))        {net_ptr->name = id(cxt);}
+    else if (accept(cxt, token_t::transition)) {transition(cxt, *net_ptr);}
+    else if (accept(cxt, token_t::place))      {place(cxt, *net_ptr);}
+    else                                       {error(cxt);}
+  };
 
   return net_ptr;
 }
